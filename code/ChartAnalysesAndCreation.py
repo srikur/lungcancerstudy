ab704304132f25de44718851c0d7055c6eb2680d
Srikur Kanuparthy <srikur@users.noreply.github.com>
2024-07-27 16:19:34 -0700

    Latest code after revisions

--- code/ChartAnalysesAndCreation.py
+++ code/ChartAnalysesAndCreation.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import stats
 import numpy as np
-from plotnine import ggplot, aes, geom_line, labs, theme_minimal, theme, geom_hline
+from plotnine import annotate, ggplot, aes, geom_line, labs, scale_color_manual, theme_minimal, theme, geom_hline, geom_smooth
 from plotnine import scale_x_continuous, scale_y_continuous, stat_smooth
 from plotnine import ggplot, aes, geom_bar, facet_wrap, labs, theme, element_text
 pd.options.mode.chained_assignment = None
@@ -34,7 +34,6 @@ p2p_ratio_male_ethnicities = lung_cancer_prevalence["Males"] / total_males * lun
 p2p_ratio_total_ethnicities = lung_cancer_prevalence["Total"] / total_total * lung_cancer_prevalence["PopPercentage"]
 
 # concat the two series into a dataframe with Ethnicity as a column
-
 p2p_ratios = pd.DataFrame({
     'Group': lung_cancer_prevalence['Group'],
     'Female_Ratio': p2p_ratio_female_ethnicities,
@@ -42,66 +41,27 @@ p2p_ratios = pd.DataFrame({
     'Total_Ratio': p2p_ratio_total_ethnicities
 })
 
-# ----------------- Minority PPR -----------------
-race_columns = ["Native American", "Black", "Other", "Asian", "Pacific", "White"]
-minority_participation_agg = df.groupby("Start Year").agg({k: "sum" for k in race_columns}).reset_index()
-minority_participation_agg = minority_participation_agg.astype(int)
-minority_participation_agg["total_participants"] = minority_participation_agg.apply("sum", axis=1) - minority_participation_agg["Start Year"]
-minority_participation_agg["minority_participants"] = minority_participation_agg["total_participants"] - minority_participation_agg["White"]
-minority_participation_agg["minority_participation_rate"] = minority_participation_agg["minority_participants"] / minority_participation_agg["total_participants"]
-minority_participation_agg["minority_ppr"] = minority_participation_agg["minority_participation_rate"] / p2p_ratios["Total_Ratio"][0:4].sum()
-minority_participation_agg = minority_participation_agg[(minority_participation_agg["Start Year"] >= 2002) & (minority_participation_agg["Start Year"] <= 2021)]
+# Calculate overall ppr point statistic for Female, Minority, Asian, Black, White, Native American, Hispanic
+group_columns = ['Female', 'minority_number', 'Asian', 'Black', 'White', 'Native American', 'Hispanic']
+overall_df = df[df["White"].notna()]
+# first calculate participation rate for each group in total across all years
+participation_rate = overall_df[group_columns].sum() / overall_df["Total Number"].sum()
+# then calculate the ppr for each group individually
+pprs = {}
+for column in group_columns:
+    if column == "minority_number":
+        pprs[column] = participation_rate[column] / p2p_ratios["Total_Ratio"][0:4].sum()
+    else:
+        pprs[column] = participation_rate[column] / p2p_ratios[p2p_ratios["Group"] == column]["Total_Ratio"].values[0]
 
-# Linear regression on minorty participation rate over time from 2002-2021
-x = minority_participation_agg["Start Year"]
-y = minority_participation_agg["minority_ppr"]
-slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
-
-# Plotting
-base_plot = (
-    ggplot(minority_participation_agg, aes(x="Start Year", y="minority_ppr")) +
-    geom_line(color="blue", size=1, na_rm=True) +
-    labs(x="Year", y="Minority PPR", title="Minority PPR by Year") +
-    theme_minimal()
-)
-
-base_plot += stat_smooth(method='lm', color='red', linetype='dashed', se=False)
-base_plot += labs(subtitle=f"p-value: {p_value.round(5)}")
-base_plot += geom_hline(yintercept=1, color='green', linetype='dashed')
-
-base_plot.save(f"{base_folder}/plots/minority_ppr_by_year.png", dpi=1200)
-# base_plot.save(f"{base_folder}/plots/minority_ppr_by_year.svg", dpi=1200)
-
-# ----------------- Female PPR -----------------
 gender_columns = ["Female", "Male"]
 gender_participation_agg = df.groupby("Start Year").agg({k: "sum" for k in gender_columns}).reset_index()
 gender_participation_agg = gender_participation_agg.astype(int)
 gender_participation_agg["total_participants"] = gender_participation_agg.apply("sum", axis=1) - gender_participation_agg["Start Year"]
 gender_participation_agg["female_participation_rate"] = gender_participation_agg["Female"] / gender_participation_agg["total_participants"]
 gender_participation_agg["female_ppr"] = gender_participation_agg["female_participation_rate"] / p2p_ratios[p2p_ratios["Group"] == "Female"]["Total_Ratio"].values[0]
-gender_participation_agg = gender_participation_agg[(minority_participation_agg["Start Year"] >= 2002) & (gender_participation_agg["Start Year"] <= 2021)]
+gender_participation_agg = gender_participation_agg[(gender_participation_agg["Start Year"] >= 2002) & (gender_participation_agg["Start Year"] <= 2021)]
 
-# Linear regression
-x = gender_participation_agg["Start Year"]
-y = gender_participation_agg["female_ppr"]
-slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
-
-# Plotting
-base_plot = (
-    ggplot(gender_participation_agg, aes(x="Start Year", y="female_ppr")) +
-    geom_line(color="blue", size=1, na_rm=True) +
-    labs(x="Year", y="Female PPR", title="Female PPR by Year") +
-    theme_minimal()
-)
-
-base_plot += stat_smooth(method='lm', color='red', linetype='dashed', se=False)
-base_plot += labs(subtitle=f"p-value: {p_value.round(5)}")
-base_plot += geom_hline(yintercept=1, color='green', linetype='dashed')
-
-base_plot.save(f"{base_folder}/plots/female_ppr_by_year.png", dpi=1200)
-# base_plot.save(f"{base_folder}/plots/female_ppr_by_year.svg", dpi=1200)
-
-# ---------------- Hispanic PPR -----------------
 hispanic_agg = df.groupby("Start Year").agg({"Hispanic": "sum", "Non-His": "sum", "Unknown Ethnicity": "sum"}).reset_index()
 hispanic_agg = hispanic_agg.astype(int)
 hispanic_agg["total_participants"] = hispanic_agg.apply("sum", axis=1) - hispanic_agg["Start Year"]
@@ -109,27 +69,47 @@ hispanic_agg["hispanic_participation_rate"] = hispanic_agg["Hispanic"] / hispani
 hispanic_agg["hispanic_ppr"] = hispanic_agg["hispanic_participation_rate"] / p2p_ratios[p2p_ratios["Group"] == "Hispanic"]["Total_Ratio"].values[0]
 hispanic_agg = hispanic_agg[(hispanic_agg["Start Year"] >= 2002) & (hispanic_agg["Start Year"] <= 2021)]
 
+# ----------------- Combined Female, Minority, Hispanic PPR by year -----------------
+combined_ppr = pd.merge(gender_participation_agg, minority_participation_agg, on="Start Year")
+combined_ppr = pd.merge(combined_ppr, hispanic_agg, on="Start Year")
+combined_ppr = combined_ppr[['Start Year', 'minority_ppr', 'hispanic_ppr', 'female_ppr']]
+combined_ppr.columns = ['Start Year', 'Minority', 'Hispanic', 'Female']
+
 # Linear regression
-x = hispanic_agg["Start Year"]
-y = hispanic_agg["hispanic_ppr"]
-slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
+x = combined_ppr["Start Year"]
+y = combined_ppr["Minority"]
+minority_pval = stats.linregress(x,y)[3]
+
+y = combined_ppr["Hispanic"]
+hispanic_pval = stats.linregress(x,y)[3]
+
+y = combined_ppr["Female"]
+female_pval = stats.linregress(x,y)[3]
 
 # Plotting
 base_plot = (
-    ggplot(hispanic_agg, aes(x="Start Year", y="hispanic_ppr")) +
-    geom_line(color="blue", size=1, na_rm=True) +
-    labs(x="Year", y="Hispanic PPR", title="Hispanic PPR by Year") +
-    theme_minimal()
+    ggplot(combined_ppr, aes(x="Start Year")) +
+    geom_line(aes(y="Minority", color="'Minority'"), size=1, na_rm=True) +
+    geom_line(aes(y="Hispanic", color="'Hispanic'"), size=1, na_rm=True) +
+    geom_line(aes(y="Female", color="'Female'"), size=1, na_rm=True) + 
+    geom_smooth(aes(y="Minority", color="'Minority'"), method='lm', linetype='dashed', se=False) +
+    geom_smooth(aes(y="Hispanic", color="'Hispanic'"), method='lm', linetype='dashed', se=False) +
+    geom_smooth(aes(y="Female", color="'Female'"), method='lm', linetype='dashed', se=False) +
+    labs(x="Year", y="PPR", title="Female, Hispanic, and Minority PPR by Year") + 
+    scale_color_manual(values={"Minority": "mediumturquoise", "Hispanic": "tomato", "Female": "purple"}) +
+    theme(legend_title=element_text(text="Group"))
 )
 
-base_plot += stat_smooth(method='lm', color='red', linetype='dashed', se=False)
-base_plot += labs(subtitle=f"p-value: {p_value.round(5)}")
+# Add p-values to the plot
+base_plot += annotate('text', x=2010, y=0.21, label=f"Female p-value: {female_pval.round(3)}", color="purple", ha='left') # type: ignore
+base_plot += annotate('text', x=2010, y=0.13, label=f"Hispanic p-value: {minority_pval.round(3)}", color="mediumturquoise", ha='left') # type: ignore
+base_plot += annotate('text', x=2010, y=0.05, label=f"Ethnicity p-value: {hispanic_pval.round(3)}", color="tomato", ha='left') # type: ignore
 base_plot += geom_hline(yintercept=1, color='green', linetype='dashed')
 
-base_plot.save(f"{base_folder}/plots/hispanic_ppr_by_year.png", dpi=1200)
-# base_plot.save(f"{base_folder}/plots/hispanic_ppr_by_year.svg", dpi=1200)
+base_plot.show()
 
-# ----------------- Statistical Tests -----------------
+base_plot.save(f"{base_folder}/plots/combined_hispanic_female_minority.png", dpi=1200)
+base_plot.save(f"{base_folder}/plots/combined_hispanic_female_minority.svg", dpi=1200)
 
 # ----------------- Surgery Type and Demographic Chart -----------------
 minority_df = df[df["White"].notna()]
@@ -170,14 +150,16 @@ melted_df['Surgery?'] = melted_df['Surgery?'].map({0: 'No', 1: 'Yes'})
 
 # Create grouped bar chart on a single axis
 base_plot = (
-    ggplot(melted_df, aes(x='Demographic', y='value', fill='Surgery?')) +
+    ggplot(melted_df, aes(x='Surgery?', y='value', fill='Demographic')) +
     geom_bar(stat='identity', position='dodge') +
-    labs(x='Demographic', y='Prevalence-to-Participation Ratio (PPR)', title='PPR by Surgery Offered and Demographic') +
+    labs(x='Surgery Offered?', y='Prevalence-to-Participation Ratio (PPR)', title='PPR by Surgery Offered and Demographic') +
     theme(axis_text_x=element_text(rotation=0))
 )
 
+base_plot.show()
+
 base_plot.save(f"{base_folder}/plots/ppr_by_surgery_and_demographic.png", dpi=1200)
-# base_plot.save(f"{base_folder}/plots/ppr_by_surgery_and_demographic.svg", dpi=1200)
+base_plot.save(f"{base_folder}/plots/ppr_by_surgery_and_demographic.svg", dpi=1200)
 
 # ----------------- Funding Type and Demographic Chart -----------------
 minority_df = df[df["White"].notna()]
@@ -209,58 +191,48 @@ melted_df['Funder Type'] = melted_df['Funder Type'].map({'OTHER': 'Other', 'INDU
 base_plot = (
     ggplot(melted_df, aes(x='Demographic', y='value', fill='Funder Type')) +
     geom_bar(stat='identity', position='dodge') +
-    labs(x='Demographic', y='Prevalence-to-Participation Ratio (PPR)', title='PPR by Funder Type and Demographic') +
+    labs(x='Funder Type', y='Prevalence-to-Participation Ratio (PPR)', title='PPR by Funder Type and Demographic') +
     theme(axis_text_x=element_text(rotation=0))
 )
 
+base_plot.show()
+
 base_plot.save(f"{base_folder}/plots/ppr_by_funder_and_demographic.png", dpi=1200)
-# base_plot.save(f"{base_folder}/plots/ppr_by_funder_and_demographic.svg", dpi=1200)
+base_plot.save(f"{base_folder}/plots/ppr_by_funder_and_demographic.svg", dpi=1200)
 
-# ----------------- Ethnicity Reporting Rate -----------------
-# Group by year and get rate of reporting for ethnicity. i.e. per year, rate of has_ethnicity == 1 vs total studies
-ethnicity_reporting_df = df.groupby("Start Year").agg({"has_ethnicity": ["mean", "count"]})
-ethnicity_reporting_df.columns = ["ethnicity_reporting_rate", "total_studies_reported_eth"]
-ethnicity_reporting_df = ethnicity_reporting_df[ethnicity_reporting_df.index >= 2002].reset_index(drop=False)
+# ----------------- Race and Ethnicity Reporting Rate -----------------
+reporting_df = df.groupby("Start Year").agg({"has_ethnicity": ["mean"], "has_race": ["mean"]})
+reporting_df.columns = ["ethnicity_reporting_rate", "race_reporting_rate"]
+reporting_df = reporting_df[reporting_df.index >= 2002].reset_index(drop=False)
 
-# Linear regression
-x = ethnicity_reporting_df["Start Year"]
-y = ethnicity_reporting_df["ethnicity_reporting_rate"]
+# Linear regression for both
+x = reporting_df["Start Year"]
+y = reporting_df["ethnicity_reporting_rate"]
 slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
+eth_pval = p_value
 
+x = reporting_df["Start Year"]
+y = reporting_df["race_reporting_rate"]
+slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+race_pval = p_value
+
+# ggplot of race and ethnicity reporting rate over time
 base_plot = (
-    ggplot(ethnicity_reporting_df, aes(x="Start Year", y="ethnicity_reporting_rate")) +
-    geom_line(color="blue", size=1, na_rm=True) +
-    labs(x="Year", y="Reporting Rate", title="Ethnicity Reporting Rate by Year") +
-    theme_minimal()
+    ggplot(reporting_df, aes(x="Start Year")) +
+    geom_line(aes(y="ethnicity_reporting_rate", color='"Ethnicity"'), size=1, na_rm=True) +
+    geom_line(aes(y="race_reporting_rate", color='"Race"'), size=1, na_rm=True) +
+    geom_smooth(aes(y="ethnicity_reporting_rate", color='"Ethnicity"'), method='lm', linetype='dashed', se=False) +
+    geom_smooth(aes(y="race_reporting_rate", color='"Race"'), method='lm', linetype='dashed', se=False) +
+    labs(x="Year", y="Reporting Rate", title="Race and Ethnicity Reporting Rate by Year") +
+    scale_color_manual(values={"Ethnicity": "mediumturquoise", "Race": "tomato"}) +
+    theme(legend_title=element_text(text="Reporting Type"))
 )
 
-base_plot += stat_smooth(method='lm', color='red', linetype='dashed', se=False)
-base_plot += labs(subtitle=f"p-value: {p_value.round(10)}")
-base_plot += geom_hline(yintercept=1, color='green', linetype='dashed')
+# Add p-values to the plot
+base_plot += annotate('text', x=2012.5, y=0.05, label=f"Ethnicity p-value: {'{:.3e}'.format(eth_pval) if eth_pval >= 0.001 else '< 0.001'}", color="mediumturquoise", ha='left') # type: ignore
+base_plot += annotate('text', x=2012.5, y=0.10, label=f"Race p-value: {'{:.3e}'.format(race_pval) if race_pval >= 0.001 else '< 0.001'}", color="tomato", ha='left') # type: ignore
 
-base_plot.save(f"{base_folder}/plots/ethnicity_reporting_rate_over_time.png", dpi=1200)
-# base_plot.save(f"{base_folder}/plots/ethnicity_reporting_rate_over_time.svg", dpi=1200)
+base_plot.show()
 
-# ---------------- Race Reporting Rate -----------------
-race_reporting_df = df.groupby("Start Year").agg({"has_race": ["mean", "count"]})
-race_reporting_df.columns = ["race_reporting_rate", "total_studies_reporting_race"]
-race_reporting_df = race_reporting_df[race_reporting_df.index >= 2002].reset_index(drop=False)
-
-# Linear regression
-x = race_reporting_df["Start Year"]
-y = race_reporting_df["race_reporting_rate"]
-slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
-
-base_plot = (
-    ggplot(race_reporting_df, aes(x="Start Year", y="race_reporting_rate")) +
-    geom_line(color="blue", size=1, na_rm=True) +
-    labs(x="Year", y="Reporting Rate", title="Race Reporting Rate by Year") +
-    theme_minimal()
-)
-
-base_plot += stat_smooth(method='lm', color='red', linetype='dashed', se=False)
-base_plot += labs(subtitle=f"p-value: {p_value.round(5)}")
-base_plot += geom_hline(yintercept=1, color='green', linetype='dashed')
-
-base_plot.save(f"{base_folder}/plots/race_reporting_rate_over_time.png", dpi=1200)
-# base_plot.save(f"{base_folder}/plots/race_reporting_rate_over_time.svg", dpi=1200)
+base_plot.save(f"{base_folder}/plots/race_and_ethnicity_reporting_rate_over_time.png", dpi=1200)
+base_plot.save(f"{base_folder}/plots/race_and_ethnicity_reporting_rate_over_time.svg", dpi=1200)
